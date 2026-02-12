5b828be54dd4e180e676d1a474e0bd256ef1a3eb
Anton <a.k_93@mail.ru>
2026-02-12 17:30:53 +0300

    Update NFTMarketplace.sol

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
     struct Listing {
