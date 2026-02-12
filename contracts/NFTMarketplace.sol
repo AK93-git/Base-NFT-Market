b94b626b90456bfddd3fa23777b23881d7dbd463
Anton <a.k_93@mail.ru>
2026-02-12 17:30:36 +0300

    Update NFTMarketplace.sol

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -9,20 +8,19 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
     struct Listing {
         address seller;
-        uint256 price; // wei 
+        uint256 price;
+        uint256 expiresAt; // 0 = no expiry
     }
 
     uint256 public nextTokenId = 1;
     mapping(uint256 => Listing) public listings;
- 
-    // Platform fee (NEW)
+
     address public feeRecipient;
-    uint96 public feeBps; // 100 = 1%, 250 = 2.5%, max 1000 = 10%
+    uint96 public feeBps; // max 1000 = 10%
 
     event Minted(address indexed to, uint256 indexed tokenId, string tokenURI);
-    event Listed(address indexed seller, uint256 indexed tokenId, uint256 price);
+    event Listed(address indexed seller, uint256 indexed tokenId, uint256 price, uint256 expiresAt);
     event Unlisted(address indexed seller, uint256 indexed tokenId);
-
     event Sold(
         address indexed buyer,
         address indexed seller,
@@ -51,9 +49,6 @@ contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
         _setFeeParams(feeRecipient_, feeBps_);
     }
 
-    // -----------------------
-    // Mint
-    // -----------------------
     function mint(address to, string calldata uri) external returns (uint256 tokenId) {
         require(to != address(0), "to=0");
         tokenId = nextTokenId++;
@@ -62,9 +57,6 @@ contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
         emit Minted(to, tokenId, uri);
     }
 
-    // -----------------------
-    // Admin
-    // -----------------------
     function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
         require(receiver != address(0), "receiver=0");
         require(feeNumerator <= 2000, "royalty too high");
@@ -84,27 +76,24 @@ contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
         emit FeeParamsUpdated(feeRecipient_, feeBps_);
     }
 
-    // -----------------------
-    // Listings
-    // -----------------------
-    function list(uint256 tokenId, uint256 price) external {
+    function list(uint256 tokenId, uint256 price, uint256 expiresAt) external {
         require(ownerOf(tokenId) == msg.sender, "not owner");
         require(price > 0, "price=0");
-
         require(
             getApproved(tokenId) == address(this) || isApprovedForAll(msg.sender, address(this)),
             "market not approved"
         );
 
-        listings[tokenId] = Listing({seller: msg.sender, price: price});
-        emit Listed(msg.sender, tokenId, price);
+        if (expiresAt != 0) require(expiresAt > block.timestamp, "expiresAt in past");
+
+        listings[tokenId] = Listing({seller: msg.sender, price: price, expiresAt: expiresAt});
+        emit Listed(msg.sender, tokenId, price, expiresAt);
     }
 
     function unlist(uint256 tokenId) external {
         Listing memory l = listings[tokenId];
         require(l.seller != address(0), "not listed");
         require(l.seller == msg.sender, "not seller");
-
         delete listings[tokenId];
         emit Unlisted(msg.sender, tokenId);
     }
@@ -114,44 +103,30 @@ contract NFTMarketplace is ERC721URIStorage, ERC2981, Ownable, ReentrancyGuard {
         require(l.seller != address(0), "not listed");
         require(l.seller != msg.sender, "seller=buyer");
         require(msg.value == l.price, "wrong value");
+        if (l.expiresAt != 0) require(block.timestamp <= l.expiresAt, "listing expired");
 
-        // Effects first
         delete listings[tokenId];
 
-        // Royalties (ERC-2981)
         (address royaltyReceiver, uint256 royaltyAmount) = royaltyInfo(tokenId, msg.value);
-
-        // Platform fee (NEW)
         uint256 platformFee = (msg.value * feeBps) / 10000;
-
-        // Seller proceeds
         uint256 sellerAmount = msg.value - royaltyAmount - platformFee;
 
-        // Transfer NFT
         _safeTransfer(l.seller, msg.sender, tokenId, "");
 
-        // Payout platform fee
         if (platformFee > 0) {
             (bool fOk, ) = payable(feeRecipient).call{value: platformFee}("");
             require(fOk, "fee transfer failed");
         }
-
-        // Payout royalty
         if (royaltyAmount > 0) {
             (bool rOk, ) = payable(royaltyReceiver).call{value: royaltyAmount}("");
             require(rOk, "royalty transfer failed");
         }
-
-        // Payout seller
         (bool sOk, ) = payable(l.seller).call{value: sellerAmount}("");
         require(sOk, "seller transfer failed");
 
         emit Sold(msg.sender, l.seller, tokenId, msg.value, platformFee, royaltyReceiver, royaltyAmount);
     }
 
-    // -----------------------
-    // Interface support
-    // -----------------------
     function supportsInterface(bytes4 interfaceId)
         public
         view
